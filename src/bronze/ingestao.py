89f0cbe107d40fbbda61e92adcea0f216ffa3a2f
WilPassion <WilPassion>
2024-11-22 14:30:32 +0000

    feat(PI4): ajustes script ingestão
    Ajustes no código

--- src/bronze/ingestao.py
+++ src/bronze/ingestao.py
@@ -1,8 +1,10 @@
 # Databricks notebook source
+# DBTITLE 1,Leitura Aquivos - Bucket
 dbutils.fs.ls("s3://translog-pb-raw/upsell/full_load/viagens/")
 
 # COMMAND ----------
 
+# DBTITLE 1,Importação Sessão Spark
 # Importando bibliotecas
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import split, col
@@ -14,14 +16,20 @@ spark = SparkSession.builder \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
     .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
     .enableHiveSupport() \
-    .getOrCreate()
+    .getOrCreate() 
 
+# COMMAND ----------
+
+# DBTITLE 1,Load dos Arquivos
 # Caminho do S3 onde estão os arquivos Parquet
 s3_path = "s3://translog-pb-raw/upsell/full_load/viagens/"
 
 # Carregar todos os arquivos Parquet do caminho
 df_full = spark.read.format("parquet").load("s3://translog-pb-raw/upsell/full_load/viagens/")
 
+# COMMAND ----------
+
+# DBTITLE 1,Corrigindo Colunas
 # Renomear colunas para evitar erros causados por caracteres especiais
 for col_name in df_full.columns:
     new_col_name = col_name.replace(" ", "_").replace(",", "_").replace(";", "_") \
@@ -40,6 +48,16 @@ if "DES_EMISSAO_VEICULO_MOTORISTA_TOT_RECEB" in df_full.columns:
                      .withColumn("TOT_RECEB", split(col("DES_EMISSAO_VEICULO_MOTORISTA_TOT_RECEB"), ",")[4]) \
                      .drop("DES_EMISSAO_VEICULO_MOTORISTA_TOT_RECEB")
 
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC catalog = "bronze"  
+# MAGIC schema = "upsell"  
+# MAGIC tablename = "viagens"  
+
+# COMMAND ----------
+
+# DBTITLE 1,Escrevendo Arquivo/Tabela Delta
 # Salvar o DataFrame como uma Delta Table na camada bronze
 (df_full.coalesce(1)
  .write
@@ -50,4 +68,3 @@ if "DES_EMISSAO_VEICULO_MOTORISTA_TOT_RECEB" in df_full.columns:
 
 # Mensagem de sucesso/confirmação
 print("Delta Table salva como bronze.upsell.viagens com sucesso!")
-
