91ebd464f78fcf19c13286405b25f82324fd9d2d
WilPassion <WilPassion>
2024-11-30 23:49:27 +0000

    feat(PI4): Atualizando delta tables e processo de ingestão

--- src/silver/distancias.sql
+++
@@ -1,8 +0,0 @@
-%sql
-SELECT
-    ID_viagem AS idViagem,
-    DISTANCIA_IDA_KM AS distanciaIdaKm,
-    DISTANCIA_VOLTA_KM AS distanciaVoltaKm,
-    DISTANCIA_IDA_KM + DISTANCIA_VOLTA_KM AS distanciaTotalKm,
-    TIPO_RODOVIA AS tipoRodovia
-FROM bronze.upsell.viagens
