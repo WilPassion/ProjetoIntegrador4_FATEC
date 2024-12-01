df776f55a5e7d91f2fa16724a0c9196c39ced785
WilPassion <WilPassion>
2024-12-01 01:16:48 +0000

    fix(PI4): ingestão camada gold
    Script ajustado para ingestão das análises mensais

--- src/gold/ingestão.py
+++ src/gold/ingestão.py
@@ -1,4 +1,5 @@
 # Databricks notebook source
+# DBTITLE 1,Ingestão - daily_report
 def import_query(path):
     with open(path, "r") as open_file:
         return open_file.read()
@@ -14,4 +15,16 @@ df.write.format("delta").mode("overwrite").saveAsTable(f"{camada}.upsell.{tablen
 
 # COMMAND ----------
 
+# DBTITLE 1,Ingestão - monthly_report
+def import_query(path):
+    with open(path, "r") as open_file:
+        return open_file.read()
 
+tablename = "monthly_report"
+camada = "gold"
+
+query = import_query(tablename + ".sql")
+print(query)
+
+df = spark.sql(query)
+df.write.format("delta").mode("overwrite").saveAsTable(f"{camada}.upsell.{tablename}")
